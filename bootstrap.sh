045dc4377fbca085188bcafaf78cda2dadd99f23
Vanessa Busch <vbusch@redhat.com>
2025-02-14 11:03:51 -0500

    fix(ci): update docker cp to only include the pwd contents
    `docker cp . ` isn't consistent.  Change it to docker cp ./. to copy the contents of the working directory without the directory itself.
    Signed-off-by: Vanessa Busch <vbusch@redhat.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -71,8 +71,8 @@ fi
 export BONFIRE_BOT="true"
 export BONFIRE_NS_REQUESTER="${JOB_NAME}-${BUILD_NUMBER}"
 # which branch to fetch cicd scripts from in bonfire repo
-export BONFIRE_REPO_BRANCH="${BONFIRE_REPO_BRANCH:-main}"
-export BONFIRE_REPO_ORG="${BONFIRE_REPO_ORG:-RedHatInsights}"
+export BONFIRE_REPO_BRANCH="${BONFIRE_REPO_BRANCH:-ci-iqe-failure}"
+export BONFIRE_REPO_ORG="${BONFIRE_REPO_ORG:-vbusch}"
 export ENABLE_TELEMETRY="true"
 SUPPORTED_CLUSTERS=('ephemeral' 'crcd')
 if [[ -z "${AVAILABLE_CLUSTERS[*]}" ]]; then
